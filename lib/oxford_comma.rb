bea118388ce366905dfac284161da838650a60fa
Matt Hough <matt@hough.tv>
2017-09-25 11:48:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,10 +6,11 @@ def oxford_comma(array)
   final_word_size = array[-1].size + 1
   new_string = array.join(", ")
   new_string = new_string.insert(-final_word_size, "and, ")
-  puts new_string
+  new_string
   #binding.pry
 end
 
 oxford_comma(array)
 
 #operate on -1 of array, find length of word and use the length of the word to go back that many chararacters and insert using .insert
+
