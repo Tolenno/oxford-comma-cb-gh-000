66f33288245439e8e334501e3c77d8bea5075b01
Matt Hough <matt@hough.tv>
2017-09-25 11:45:00 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 array = ["fiddleheads","okra","kohlrabi"]
 
 def oxford_comma(array)
+  array_size = array[-1].size
+  
   new_string = array.join(", ")
   new_string = new_string.insert(-9, "and, ")
   puts new_string
