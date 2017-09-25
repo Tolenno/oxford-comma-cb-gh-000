e9dce421d7647a60b07fb024c0919bb4c6c17530
Matt Hough <matt@hough.tv>
2017-09-25 11:49:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,10 +3,12 @@ require "pry"
 array = ["fiddleheads","okra","kohlrabi"]
 
 def oxford_comma(array)
-  final_word_size = array[-1].size + 1
-  new_string = array.join(", ")
-  new_string = new_string.insert(-final_word_size, "and, ")
-  new_string
+  if array.size > 2
+    final_word_size = array[-1].size + 1
+    new_string = array.join(", ")
+    new_string = new_string.insert(-final_word_size, "and, ")
+    new_string
+  end
   #binding.pry
 end
 
