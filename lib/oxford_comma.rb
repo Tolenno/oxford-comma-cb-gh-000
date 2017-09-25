dc4c2503142fd38097ce7274caedb842d5d36717
Matt Hough <matt@hough.tv>
2017-09-25 11:32:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,11 @@ array = ["fiddleheads","okra","kohlrabi"]
 
 def oxford_comma(array)
   new_string = array.join(", ")
+  new_string = new_string.insert(-2, "and")
   puts new_string
 end
 
 oxford_comma(array)
+
+
+.insert(-2, word) #act on an array
