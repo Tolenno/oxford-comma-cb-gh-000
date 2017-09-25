bfb76558c602fbca7fd42bad7c8d383a8dd2a865
Matt Hough <matt@hough.tv>
2017-09-25 11:40:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,10 @@ array = ["fiddleheads","okra","kohlrabi"]
 
 def oxford_comma(array)
   new_string = array.join(", ")
-  new_string = new_string.insert(-2, "and")
+  new_string = new_string.insert(-9, "and")
   puts new_string
 end
 
 oxford_comma(array)
+
+operate on -1 of array, find length of word and use the length of the word to go back that many chararacters and insert using .insert
